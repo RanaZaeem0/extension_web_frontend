ec11a3b9930ee5972bb47401ebac0faf0c3057fb
Shamoil Khan <shamoilkhan210@gmail.com>
2025-02-22 10:32:50 +0500

    designed UI for whatsapp extension

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,38 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { ProtectRoute } from "./components/auth/ProtectRoute";
 const Pricing = lazy(() => import("./pages/pricing/Pricing"));
 const Login = lazy(() => import("./pages/login/Login"));
-import LayoutLoader from "./components/LayoutLoader";
+import LayoutLoader from "./components/LayoutLoader"
 import NotFound from "./pages/NotFound";
 import Dashboard from "./pages/dashborad/Dashborad";
+import Setting from "./pages/setting/setting";
+import Home from "./pages/home/Home";
+import "./App.css"
 function App() {
-  const user = [{}];
+
+
+  const user =[{}]
+
 
   return (
     <BrowserRouter>
       <Suspense fallback={<LayoutLoader />}>
         <Routes>
-          <Route element={<ProtectRoute user={user} />}>
+          <Route
+            element={
+            
+                <ProtectRoute user={user} />
+              
+            }
+          >
+            <Route path="/" element={<Home />} />
             <Route path="/pricing" element={<Pricing />} />
             <Route path="/profile" element={<Dashboard />} />
+            <Route path="/profile/settings" element={<Setting />} />
           </Route>
-
           <Route
             path="/login"
             element={
-              <ProtectRoute user={!user} redirect="/">
+            <ProtectRoute user={!user} redirect="/">
                 <Login />
               </ProtectRoute>
             }
