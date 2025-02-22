9c48f4a22a974b3e6e8c836a7e8a401ac8777907
Shamoil Khan <shamoilkhan210@gmail.com>
2025-02-22 10:25:47 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,24 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { ProtectRoute } from "./components/auth/ProtectRoute";
 const Pricing = lazy(() => import("./pages/pricing/Pricing"));
 const Login = lazy(() => import("./pages/login/Login"));
-import LayoutLoader from "./components/LayoutLoader"
+import LayoutLoader from "./components/LayoutLoader";
 import NotFound from "./pages/NotFound";
 import Dashboard from "./pages/dashborad/Dashborad";
 function App() {
-
-
-  const user =[{}]
-
+  const user = [{}];
 
   return (
     <BrowserRouter>
       <Suspense fallback={<LayoutLoader />}>
         <Routes>
-          <Route
-            element={
-            
-                <ProtectRoute user={user} />
-              
-            }
-          >
+          <Route element={<ProtectRoute user={user} />}>
             <Route path="/pricing" element={<Pricing />} />
             <Route path="/profile" element={<Dashboard />} />
           </Route>
           <Route
             path="/login"
             element={
-            <ProtectRoute user={!user} redirect="/">
+              <ProtectRoute user={!user} redirect="/">
                 <Login />
               </ProtectRoute>
             }
